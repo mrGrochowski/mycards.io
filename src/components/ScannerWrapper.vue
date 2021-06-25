945741dcb2b99b260b7c970bd64848b24135caab
Mateusz <mateuszgrochowski@divante.pl>
2021-06-25 15:09:39 +0200

    remove unessesary code

--- src/components/ScannerWrapper.vue
+++ src/components/ScannerWrapper.vue
@@ -10,12 +10,13 @@
     </div>
 
     <div>
-      <video id="video" ref="video" width="300" height="200" style="border: 1px solid gray"></video>
-    </div>
-
-    <div ref="sourceSelectPanel">
-      <label for="sourceSelect">Change video source:</label>
-      <select id="sourceSelect" ref="sourceSelect" style="max-width: 400px"></select>
+      <video
+        id="video"
+        ref="videoV"
+        width="300"
+        height="200"
+        style="border: 1px solid gray"
+      ></video>
     </div>
 
     <label>Result:</label>
@@ -35,59 +36,29 @@
 import { BrowserMultiFormatReader, BarcodeFormat } from '@zxing/library'
 import { ref, onMounted } from 'vue'
 
-const codeReader = new BrowserMultiFormatReader()
-
-const sourceSelect = ref(null)
 const startButton = ref(null)
 const resetButton = ref(null)
-const video = ref(null)
+const videoV = ref(null)
 const resultV = ref(null)
 
+const codeReader = new BrowserMultiFormatReader()
+
 let selectedDeviceId
-console.clear()
 onMounted(() => {
-  codeReader
-    .listVideoInputDevices()
-    .then((videoInputDevices) => {
-      const sourceSelectV = sourceSelect.value
-      selectedDeviceId = videoInputDevices[0].deviceId
-      if (videoInputDevices.length >= 1) {
-        videoInputDevices.forEach((element) => {
-          const sourceOption = document.createElement('option')
-          sourceOption.text = element.label
-          sourceOption.value = element.deviceId
-          sourceSelectV.appendChild(sourceOption)
-        })
-
-        sourceSelectV.onchange = () => {
-          selectedDeviceId = sourceSelect.value
-        }
-
+  startButton.value.addEventListener('click', () => {
+    codeReader.decodeFromVideoDevice('', videoV.value, (result, err) => {
+      if (result) {
+        resultV.value.textContent = result
       }
-
-      startButton.value.addEventListener('click', () => {
-        codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
-          if (result) {
-            //resultV.value.textContent = result.text
-            resultV.value.textContent = result
-          }
-          // if (err) {
-          //   console.error(err)
-          //   resultV.value.textContent = err
-          // }
-        })
-        console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
-      })
-
-      resetButton.value.addEventListener('click', () => {
-        codeReader.reset()
-        result.textContent = ''
-        console.log('Reset.')
-      })
     })
-    .catch((err) => {
-      console.error(err)
-    })
+    console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+  })
+
+  resetButton.value.addEventListener('click', () => {
+    codeReader.reset()
+    result.textContent = ''
+    console.log('Reset.')
+  })
 })
 </script>
 
