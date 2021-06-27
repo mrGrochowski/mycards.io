1a672d0815bf81b4d1b8db4a777e926a0631760f
Mateusz <mateuszgrochowski@divante.pl>
2021-06-27 12:37:56 +0200

    fix few things

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <Landing />
-  <Nav />
+  <div class="bg-gradient-to-r from-gray-600 to-gray-500">
+    <Landing />
+    <Nav />
+  </div>
 </template>
 
 <script setup>
@@ -9,3 +11,5 @@ import Nav from './components/Nav.vue'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
+<style>
+</style>
--- src/components/ScannerWrapper.vue
+++ src/components/ScannerWrapper.vue
@@ -1,57 +1,62 @@
 ﻿<template>
-<div
-    class="
-      min-h-screen
-      flex flex-shrink-0 flex-grow-0 flex-col
-      justify-center
-      items-center
-      bg-gradient-to-r
-      from-gray-600
-      to-gray-500
-    "
-  >
-    <a
-      @click="callStartScanning"
+  <div class="relative h-screen w-screen flex justify-center items-center">
+    <video
+      ref="videoV"
+      class="h-screen z-0 w-screen absolute select-none pointer-events-none object-cover"
+    ></video>
+    <div
       class="
-        bg-blue-500
+        inline-flex w-3/4 h-4/4
+        flex-shrink-0 flex-grow-0 flex-wrap
+        justify-center
+        items-center
+        z-10
+        relative
+        p-9
         rounded-lg
-        font-bold
-        text-white text-center
-        px-4
-        py-3
-        transition
-        duration-300
-        ease-in-out
-        hover:bg-blue-600
+        specialGradient
       "
     >
-      Dodaj pierwszą kartę
-    </a>
-    <a
-      @click="callResetScanning"
-      class="
-        bg-blue-800
-        rounded-lg
-        font-bold
-        text-white text-center
-        px-4
-        py-3
-        transition
-        duration-300
-        ease-in-out
-        hover:bg-blue-600
-      "
-    >
-      Resetuj
-    </a>
-      <video
-        ref="videoV"
-        class="vid"
-      ></video>
-
-    <label>Result:</label>
-    <code ref="resultV"></code>
-
+      <h2 class="text-white text-4xl">Skan karty</h2>
+      <a
+        @click="callStartScanning"
+        class="
+          bg-blue-600
+          rounded-lg
+          font-bold
+          text-white text-center
+          px-4
+          py-3
+          transition
+          duration-300
+          ease-in-out
+          hover:bg-blue-700
+        "
+      >
+        Dodaj pierwszą kartę
+      </a>
+      <a
+        @click="callResetScanning"
+        class="
+          bg-blue-500
+          rounded-lg
+          font-bold
+          text-white text-center
+          px-4
+          py-3
+          transition
+          duration-300
+          ease-in-out
+          hover:bg-blue-600
+        "
+      >
+        Resetuj
+      </a>
+      <div class="align-bottom justify-end w-auto min-w-3/4 h-16 p-4">
+        <label>Result:</label>
+        <code class="w-2/3 inline-block" ref="resultV"></code>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -60,14 +65,12 @@ import { BrowserMultiFormatReader } from '@zxing/library'
 import { ref, onMounted } from 'vue'
 
 const startScanning = (codeReader, videoV, resultV) => {
-
   codeReader.decodeFromVideoDevice('', videoV, (result, err) => {
     if (result) {
       resultV.textContent = result.text
     }
   })
   console.log(`Started continous decode from camera with id `)
-
 }
 
 const resetScanning = (codeReader, resultV) => {
@@ -75,7 +78,6 @@ const resetScanning = (codeReader, resultV) => {
   codeReader.reset()
   resultV.textContent = ''
   console.log('Reset.')
-
 }
 export default {
   setup() {
@@ -83,25 +85,30 @@ export default {
     const resultV = ref(null)
     const codeReader = new BrowserMultiFormatReader()
 
-    const callStartScanning = ()=> startScanning(codeReader, videoV.value, resultV.value)
-    const callResetScanning = ()=> resetScanning(codeReader, resultV.value)
+    const callStartScanning = () => startScanning(codeReader, videoV.value, resultV.value)
+    const callResetScanning = () => resetScanning(codeReader, resultV.value)
 
     return {
       callStartScanning,
       callResetScanning,
-      videoV,resultV
+      videoV,
+      resultV,
     }
   },
 }
 </script>
 
 <style scoped>
-.container {
-  display: inline-block;
+.z--1 {
+  z-index: -1;
 }
-
-.vid {
-  width:50vw;
-  height:50vh
+.specialGradient {
+  background: linear-gradient(
+    43deg,
+    rgba(55, 59, 68, 0.432) 69%,
+    rgba(153, 150, 201, 0.342) 100%
+  );
+  backdrop-filter: blur(10px);
+  box-shadow: 0px 0px 40px rgba(67, 65, 87, 0.63);
 }
 </style>
