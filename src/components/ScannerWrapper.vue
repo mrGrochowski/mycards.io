07a138ed94ffb0657914e774cbc639a0d5173c39
Mateusz <mateuszgrochowski@divante.pl>
2021-06-30 01:49:08 +0200

    add basic pouchdb

--- src/components/ScannerWrapper.vue
+++ src/components/ScannerWrapper.vue
@@ -1,12 +1,61 @@
 ﻿<template>
-  <div class="container">
-    SOMETHING
-
-    <h1 class="title">Scan 1D/2D Code from Video Camera</h1>
-
-    <div>
-      <a class="button" ref="startButton">Start</a>
-      <a class="button" ref="resetButton">Reset</a>
+  <div class="relative h-screen w-screen flex justify-center items-center">
+    <video
+      ref="videoV"
+      class="h-screen z-0 w-screen absolute select-none pointer-events-none object-cover"
+    ></video>
+    <div
+      class="
+        inline-flex w-3/4 h-4/6 sm:h-1/2 sm:w-1/2
+        flex-shrink-0 flex-grow-0 flex-wrap
+        justify-center
+        items-center
+        z-10
+        relative
+        p-9 
+        rounded-lg
+        specialGradient
+      "
+    >
+      <h2 class="text-white text-4xl">Skan karty</h2>
+      <a
+        @click="callStartScanning"
+        class="
+          bg-blue-600
+          rounded-lg
+          font-bold
+          text-white text-center
+          px-4
+          py-3
+          transition
+          duration-300
+          ease-in-out
+          hover:bg-blue-700
+        "
+      >
+        Dodaj pierwszą kartę
+      </a>
+      <a
+        @click="callResetScanning"
+        class="
+          bg-blue-500
+          rounded-lg
+          font-bold
+          text-white text-center
+          px-4
+          py-3
+          transition
+          duration-300
+          ease-in-out
+          hover:bg-blue-600
+        "
+      >
+        Resetuj
+      </a>
+      <div class="align-bottom justify-end w-auto min-w-3/4 h-16 p-4">
+        <label>Result:</label>
+        <code class="w-2/3 inline-block" ref="resultV"></code>
+      </div>
     </div>
 
     <div>
@@ -32,14 +81,18 @@
   </div>
 </template>
 
-<script setup>
-import { BrowserMultiFormatReader, BarcodeFormat } from '@zxing/library'
+<script>
+import {setBase , writeToBase, getFromBase} from "../composables/storage.vue"
+import { BrowserMultiFormatReader } from '@zxing/library'
 import { ref, onMounted } from 'vue'
 
-const startButton = ref(null)
-const resetButton = ref(null)
-const videoV = ref(null)
-const resultV = ref(null)
+const startScanning = (codeReader, videoV, resultV) => {
+  codeReader.decodeFromVideoDevice('', videoV, (result, err) => {
+    if (result) {
+      resultV.textContent = result.text
+    }
+  })
+}
 
 const codeReader = new BrowserMultiFormatReader()
 
@@ -54,12 +107,22 @@ onMounted(() => {
     console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
   })
 
-  resetButton.value.addEventListener('click', () => {
-    codeReader.reset()
-    result.textContent = ''
-    console.log('Reset.')
-  })
-})
+    setBase()
+    //writeToBase()
+    getFromBase()
+    onMounted(()=>{
+
+
+      //callStartScanning()
+    })
+    return {
+      callStartScanning,
+      callResetScanning,
+      videoV,
+      resultV,
+    }
+  },
+}
 </script>
 
 <style scoped>
---
+++ src/composables/storage.vue
@@ -0,0 +1,37 @@
+﻿<script>
+import { PouchDB } from '@svouch/pouchdb'
+
+const db = new PouchDB('test_db1')
+export const setBase = () => {
+
+  db.info((err, info) => {
+    if (!err) {
+      console.log(info)
+    }
+  })
+}
+
+export const writeToBase = ()=> {
+
+  const testowe = {
+    _id: 'cos',
+    title: "text",
+    completed: false
+  };
+
+  db.put(testowe, function callback(err, result) {
+    if (!err) {
+      console.log('Successfully posted a todo!');
+    }
+  });
+
+}
+
+
+export const  getFromBase = async ()=>{
+
+  const result = await db.get('cos')
+  console.log(result);
+
+}
+</script>
